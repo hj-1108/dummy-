f39d5d92691f4ed94275e9e4373e6d3ee552b595
hj-1108 <hj2811@srmist.edu.in>
2026-03-09 21:40:20 +0530

    UC6

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,59 @@
+public class OOPSBannerApp6 {
+
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                "  ***  ",
+                " *   * ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " *   * ",
+                "  ***  "
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****",
+                " *     ",
+                " *     ",
+                "  ***  ",
+                "     * ",
+                "     * ",
+                " ***** "
+        };
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+
+        // Print OOPS Banner
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
+        }
+    }
+}
